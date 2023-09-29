eef58f570faf599bf26aa38398bedfcb2126426a
AnupamaCodippily <avcodippily@gmail.com>
2023-09-29 14:01:29 +0530

    remove redundant classes

--- src/main/java/io/avcode/ApacheCommonsPerformanceChecker.java
+++
@@ -1,8 +0,0 @@
-package io.avcode;
-
-public class ApacheCommonsPerformanceChecker implements PerformanceChecker{
-    @Override
-    public void compress1MB1MilTimes() {
-
-    }
-}
--- src/main/java/io/avcode/Zip4jPerformanceChecker.java
+++
@@ -1,8 +0,0 @@
-package io.avcode;
-
-public class Zip4jPerformanceChecker implements PerformanceChecker{
-    @Override
-    public void compress1MB1MilTimes() {
-
-    }
-}

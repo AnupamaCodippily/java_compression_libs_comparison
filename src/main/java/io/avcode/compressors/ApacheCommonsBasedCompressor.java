145ba0b44b17fdd7946c82dbcf0b747d940db76d
AnupamaCodippily <avcodippily@gmail.com>
2023-09-29 13:59:54 +0530

    add a compressor using apache commons

---
+++ src/main/java/io/avcode/compressors/ApacheCommonsBasedCompressor.java
@@ -0,0 +1,35 @@
+package io.avcode.compressors;
+
+import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream;
+
+import java.io.*;
+
+public class ApacheCommonsBasedCompressor implements Compressor{
+
+    @Override
+    public void compress(File file) {
+        try(FileInputStream fileInputStream = new FileInputStream(file);
+            ByteArrayOutputStream outputStream     = new ByteArrayOutputStream();
+            GzipCompressorOutputStream gzipOutputStream = new GzipCompressorOutputStream (outputStream)
+        ) {
+            byte[] data = fileInputStream.readAllBytes();
+            gzipOutputStream.write(data);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    @Override
+    public void compressToFile(File file) throws IOException {
+        File outFile = new File(Compressor.makeCompressedFilePath(file));
+        try(FileInputStream fileInputStream = new FileInputStream(file);
+            FileOutputStream outputStream     = new FileOutputStream(outFile);
+            GzipCompressorOutputStream gzipOutputStream = new GzipCompressorOutputStream (outputStream)
+        ) {
+            byte[] data = fileInputStream.readAllBytes();
+            gzipOutputStream.write(data);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+}
